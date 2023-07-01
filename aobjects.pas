1875fe76b81c783772ad3968d378d809d7649137
Ori <58131842+itokhov@users.noreply.github.com>
2023-07-01 22:23:03 +0300

    attack or run!
    added a simple attack function and implemented a mess, but working AI for monsters(tacking and attacking the hero). Oh, and if you're squeezing a key other than moving, the character doesn't do anything.

--- aobjects.pas
+++ aobjects.pas
@@ -1,5 +1,6 @@
 ﻿unit aobjects;
 uses perss;
+uses game_consts;
 
 var not_destroy: array of Not_destroyble; var nd_len:=0; //type 0
 var live_objs: array of Anger; var lo_len:=0; //type 1
@@ -8,7 +9,6 @@ var hero: Person:=new Person;
   
 var o_id:integer;
 var o_type:integer;
-type tile_m=array [1..2] of integer;
 
 function new_object(type_obj:integer; type_id:integer): integer;
 begin
@@ -17,7 +17,7 @@ begin
     if type_id=0 then //bedrock
       begin SetLength(not_destroy, nd_len); not_destroy[nd_len-1]:=new Not_destroyble('#', 0, 15); end
     else if type_id=1 then //air
-      begin SetLength(not_destroy, nd_len); not_destroy[nd_len-1]:=new Not_destroyble(' ', 0, 0, true); end; 
+      begin SetLength(not_destroy, nd_len); not_destroy[nd_len-1]:=new Not_destroyble(' ', 0, 0, true, false); end; 
     result:=nd_len-1;
     end;
       
@@ -35,9 +35,9 @@ begin
     begin 
       oo_len+=1;
       if type_id=1 then //wood
-      begin SetLength(other_objs, oo_len); other_objs[oo_len-1]:=new Block('=', 6); end; 
+      begin SetLength(other_objs, oo_len); other_objs[oo_len-1]:=new Block('=', true, 6); end; 
       if type_id=2 then //point
-      begin SetLength(other_objs, oo_len); other_objs[oo_len-1]:=new Block('.', 8); end; 
+      begin SetLength(other_objs, oo_len); other_objs[oo_len-1]:=new Block('.', true, 8); end; 
       result:=oo_len-1;
     end; end;
 
@@ -73,6 +73,44 @@ begin
   begin
     if k[2]=-1 then result:=aobjects.hero.sym
     else result:=aobjects.other_objs[k[2]].sym;
+  end;
+end;
+function get_obj_touchable(k:tile_m): boolean;
+begin
+  if k[1]=0 then begin result:=aobjects.not_destroy[k[2]].touchable; end; //dot forget about "now_color"
+  if k[1]=1 then begin result:=aobjects.live_objs[k[2]].touchable; end;
+  if k[1]=2 then 
+  begin
+    if k[2]=-1 then result:=aobjects.hero.touchable
+    else result:=aobjects.other_objs[k[2]].touchable;
+  end;
+end;
+function get_obj_visible(k:tile_m): boolean;
+begin
+  if k[1]=0 then begin result:=aobjects.not_destroy[k[2]].visible; end; //dot forget about "now_color"
+  if k[1]=1 then begin result:=aobjects.live_objs[k[2]].visible; end;
+  if k[1]=2 then 
+  begin
+    if k[2]=-1 then result:=aobjects.hero.visible
+    else result:=aobjects.other_objs[k[2]].visible;
+  end;
+end;
+function get_obj_atk(k:tile_m): integer;
+begin
+  if k[1]=1 then begin result:=aobjects.live_objs[k[2]].atk; end;
+  if k[1]=2 then 
+  begin
+    if k[2]=-1 then result:=aobjects.hero.atk
+  end;
+end;
+procedure attack_on(ater:tile_m; ator:tile_m) ;
+begin
+  var atk:=get_obj_atk(ater);
+  if ator[1]=1 then begin aobjects.live_objs[ator[2]].hp-=atk; end;
+  if ator[1]=2 then 
+  begin
+    if ator[2]=-1 then aobjects.hero.hp-=atk
+    else aobjects.other_objs[ator[2]].hp-=atk;
   end;
 end;
 
